6c5b40c631952eee946720f9d9cfe4b31ea79a86
stefaleon <stefaleon@gmail.com>
2017-05-04 23:11:27 +0300

    Add the Duck class

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 &nbsp;
 ## 00 Start the project
 * In VS, create a Console Application project.
+
+&nbsp;
+## 01 Add the Duck class
+* Add the Duck class and the KindOfDuck enum.
---
+++ Sorting Ducks/Duck.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Ducks
+{
+    class Duck
+    {
+        public int Size;
+        public KindOfDuck Kind;
+    }
+
+    enum KindOfDuck
+    {
+        Mallard,
+        Muscovy,
+        Decoy,
+    }
+}
